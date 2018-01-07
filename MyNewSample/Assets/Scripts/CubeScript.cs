8d47700299ac92928c4c7e20d354302de8a6e3ba
neighborpil <neighborpil@naver.com>
2018-01-08 05:50:06 +0900

    Lec01 finished

---
+++ MyNewSample/Assets/Scripts/CubeScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        transform.Translate(0, 0, 1.0F);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
