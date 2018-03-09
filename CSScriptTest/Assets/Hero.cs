9f62cdb4cbd510138a66aa21f03b2f064979c471
neighborpil <neighborpil@naver.com>
2018-03-09 12:47:33 +0900

    ch2 lec47 finished

---
+++ CSScriptTest/Assets/Hero.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour {
+
+    public GameObject[] enemies;
+    int count = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        if(enemies[count].GetComponent<Renderer>().material.color != Color.red)
+        {
+            enemies[count].GetComponent<Renderer>().material.color = Color.red;
+            count++;
+        }
+        else
+        {
+            enemies[count].GetComponent<Renderer>().material.color = Color.green;
+            count++;
+        }
+        if (count > enemies.Length - 1)
+            count = 0;
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
