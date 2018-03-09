d0681b350b2ea160b7ce2995158e1ae6cacca47c
neighborpil <neighborpil@naver.com>
2018-03-09 09:08:36 +0900

    ch2 lec 28 finished

---
+++ CSScriptTest/Assets/Sample4.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sample4 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        print("down");
+    }
+
+
+    private void OnMouseUp()
+    {
+        print("up");
+    }
+
+    private void OnMouseEnter()
+    {
+        print("Mouse Entered");
+    }
+
+    private void OnMouseExit()
+    {
+        print("mouse out");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
